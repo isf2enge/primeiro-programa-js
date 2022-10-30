925e67c0241c5680772e7d4446bece86e569d4bd
isf2enge <isf2@discente.ifpe.edu.br>
2022-10-30 16:17:06 -0300

    origen

---
+++ novo.js
@@ -0,0 +1,10 @@
+//Escreva uma função que usa dois números,e retorna
+//o maior entre eles.
+
+function retornaValor(x,y){
+
+    let c = x>y ? x:y;
+    return c;
+}
+
+console.log(retornaValor(8,4));
